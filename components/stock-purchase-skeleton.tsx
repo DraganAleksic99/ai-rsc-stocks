351f538eeb3ef4ab494d5d60fac7ec89938a8d20
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-06-15 15:02:11 +0200

    Add stock-purchase-skeleton component

---
+++ components/stock-purchase-skeleton.tsx
@@ -0,0 +1,24 @@
+export default function StockPurchaseSkeleton() {
+  return (
+    <div className="bg-zinc-950 rounded-lg px-4 py-5 text-center text-xs animate-pulse">
+      <div className="inline-block float-right px-2 py-1 text-xs text-transparent rounded-full bg-white/10">
+        xxxx
+      </div>
+      <div className="text-lg text-transparent bg-white/10 w-fit rounded-md m-1 mb-2">
+        xxxx
+      </div>
+      <div className="text-3xl text-transparent bg-white/10 w-fit rounded-md m-1">
+        xxxx
+      </div>
+      <div className="text-transparent bg-white/10 w-fit m-1 mt-7 rounded-full">
+        xxxxxx xxxx xxxxxx
+      </div>
+      <div className="w-full m-1 rounded-md bg-white/10 pb-6 mt-4"></div>
+      <div className="text-transparent rounded-full m-1 w-fit bg-white/10 mt-6 mb-3">
+        xxxxx xxxxx
+      </div>
+      <div className="w-full bg-white/10 rounded-md m-1 pb-10 mb-6"></div>
+      <div className="w-full bg-white/10 rounded-md m-1 pb-10"></div>
+    </div>
+  );
+}
